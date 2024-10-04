ea1ad5adf37041b846d8deb2274d5819204688ed
Gozatila <rogerinho123759@gmail.com>
2024-10-04 09:57:46 -0300

    update 3

--- ApiDaora/Program.cs
+++ ApiDaora/Program.cs
@@ -12,14 +12,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options=> {
+    options.AddPolicy("AllowCrossOrigin", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} */
 
 app.UseHttpsRedirection();
 
